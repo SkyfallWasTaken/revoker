d8be9054312a76f8777a4ecc1bc45ede027245f4
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-29 16:41:59 -0500

    hka_!

--- app/models/token_types.rb
+++ app/models/token_types.rb
@@ -2,7 +2,7 @@ module TokenTypes
   # Registry of all token types
   ALL = [
     AirtablePAT,
-    # HackatimeAdminKey, # waiting on PR hackatime/#747
+    HackatimeAdminKey,
     HCBOAuth,
     SlackXoxb,
     SlackXoxp,
