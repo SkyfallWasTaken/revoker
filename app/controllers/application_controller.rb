5fb519851764bf0bfd369eacb3732e91d34b3eb2
24c02 <163450896+24c02@users.noreply.github.com>
2025-12-28 20:20:13 -0500

    fix 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,17 @@ class ApplicationController < ActionController::Base
 include Pundit::Authorization
 
 rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+rescue_from Norairrecord::RecordNotFoundError, with: :record_not_found
 
 private
 
 def user_not_authorized
   flash[:alert] = "You are not authorized to perform this action."
   redirect_back fallback_location: root_path
+end
+
+def record_not_found
+  flash[:alert] = "The record you're looking for could not be found."
+  redirect_to root_path
 end
 end
